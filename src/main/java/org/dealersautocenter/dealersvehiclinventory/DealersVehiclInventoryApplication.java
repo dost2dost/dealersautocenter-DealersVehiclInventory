de2b669c1e072bc2679242b614ac99b87c4ed9e5
DostM <dostm@DostMs-MacBook-Air.local>
2026-04-13 21:08:12 +0500

    initial commit

---
+++ src/main/java/org/dealersautocenter/dealersvehiclinventory/DealersVehiclInventoryApplication.java
@@ -0,0 +1,13 @@
+package org.dealersautocenter.dealersvehiclinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealersVehiclInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealersVehiclInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dealersautocenter/dealersvehiclinventory/DealersVehiclInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dealersautocenter.dealersvehiclinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealersVehiclInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
