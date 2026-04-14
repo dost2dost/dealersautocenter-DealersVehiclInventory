1bb31c13fcfb98c10dd2820ec6a535442b3317b5
DostM <dostm@DostMs-MacBook-Air.local>
2026-04-14 19:37:52 +0500

    addition of ADMIN_GLOBAL Role

--- src/main/java/org/dealersautocenter/dealersvehiclinventory/security/SecurityConfig.java
+++ src/main/java/org/dealersautocenter/dealersvehiclinventory/security/SecurityConfig.java
@@ -1,26 +1,56 @@
 package org.dealersautocenter.dealersvehiclinventory.security;
 
+import lombok.RequiredArgsConstructor;
 import org.dealersautocenter.dealersvehiclinventory.config.TenantFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final TenantFilter tenantFilter;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, TenantFilter tenantFilter) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
+            .httpBasic(withDefaults())
             .authorizeHttpRequests(authz -> authz
+                    .requestMatchers("/admin/**").hasRole("GLOBAL_ADMIN")
                 .anyRequest().permitAll()
             )
             .addFilterBefore(tenantFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("GLOBAL_ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
